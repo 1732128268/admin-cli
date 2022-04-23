3bf34807b00f8fdfb639d04ee7c49e94249f64ce
zxy <1732128268@qq.com>
2022-04-23 12:16:40 +0800

    增加utils 文件上传

--- config/config.go
+++ config/config.go
@@ -3,8 +3,9 @@ package config
 type HttpConfig struct {
 	Port        int    `json:"port"`
 	Mode        string `json:"mode"`
-	OpenRedis   bool   `json:"openRedis"`   //是否开启redis
-	OpenCaptcha bool   `json:"openCaptcha"` //是否开启验证码
+	OpenRedis   bool   `json:"openRedis"`                                        //是否开启redis
+	OpenCaptcha bool   `json:"openCaptcha"`                                      //是否开启验证码
+	OssType     string `mapstructure:"oss-type" json:"oss-type" yaml:"oss-type"` // Oss类型
 }
 
 type Mysql struct {
@@ -47,6 +48,45 @@ type Casbin struct {
 	ModelPath string `mapstructure:"model-path" json:"model-path" yaml:"model-path"` // 存放casbin模型的相对路径
 }
 
+type AliyunOSS struct {
+	Endpoint        string `mapstructure:"endpoint" json:"endpoint" yaml:"endpoint"`
+	AccessKeyId     string `mapstructure:"access-key-id" json:"access-key-id" yaml:"access-key-id"`
+	AccessKeySecret string `mapstructure:"access-key-secret" json:"access-key-secret" yaml:"access-key-secret"`
+	BucketName      string `mapstructure:"bucket-name" json:"bucket-name" yaml:"bucket-name"`
+	BucketUrl       string `mapstructure:"bucket-url" json:"bucket-url" yaml:"bucket-url"`
+	BasePath        string `mapstructure:"base-path" json:"base-path" yaml:"base-path"`
+}
+
+type HuaWeiObs struct {
+	Path      string `mapstructure:"path" json:"path" yaml:"path"`
+	Bucket    string `mapstructure:"bucket" json:"bucket" yaml:"bucket"`
+	Endpoint  string `mapstructure:"endpoint" json:"endpoint" yaml:"endpoint"`
+	AccessKey string `mapstructure:"access-key" json:"access-key" yaml:"access-key"`
+	SecretKey string `mapstructure:"secret-key" json:"secret-key" yaml:"secret-key"`
+}
+type Qiniu struct {
+	Zone          string `mapstructure:"zone" json:"zone" yaml:"zone"`                                  // 存储区域
+	Bucket        string `mapstructure:"bucket" json:"bucket" yaml:"bucket"`                            // 空间名称
+	ImgPath       string `mapstructure:"img-path" json:"img-path" yaml:"img-path"`                      // CDN加速域名
+	UseHTTPS      bool   `mapstructure:"use-https" json:"use-https" yaml:"use-https"`                   // 是否使用https
+	AccessKey     string `mapstructure:"access-key" json:"access-key" yaml:"access-key"`                // 秘钥AK
+	SecretKey     string `mapstructure:"secret-key" json:"secret-key" yaml:"secret-key"`                // 秘钥SK
+	UseCdnDomains bool   `mapstructure:"use-cdn-domains" json:"use-cdn-domains" yaml:"use-cdn-domains"` // 上传是否使用CDN上传加速
+}
+
+type TencentCOS struct {
+	Bucket     string `mapstructure:"bucket" json:"bucket" yaml:"bucket"`
+	Region     string `mapstructure:"region" json:"region" yaml:"region"`
+	SecretID   string `mapstructure:"secret-id" json:"secret-id" yaml:"secret-id"`
+	SecretKey  string `mapstructure:"secret-key" json:"secret-key" yaml:"secret-key"`
+	BaseURL    string `mapstructure:"base-url" json:"base-url" yaml:"base-url"`
+	PathPrefix string `mapstructure:"path-prefix" json:"path-prefix" yaml:"path-prefix"`
+}
+
+type Local struct {
+	Path string `mapstructure:"path" json:"path" yaml:"path"` // 本地文件路径
+}
+
 type Config struct {
 	HttpConfig HttpConfig `json:"httpConfig"`
 	Redis      Redis      `json:"redis"`
@@ -56,6 +96,11 @@ type Config struct {
 	Salt       string     `json:"salt"` //加密盐
 	Casbin     Casbin     `json:"casbin" yaml:"casbin"`
 	Captcha    Captcha    `json:"captcha" yaml:"captcha"`
+	Local      Local      `mapstructure:"local" json:"local" yaml:"local"`
+	Qiniu      Qiniu      `mapstructure:"qiniu" json:"qiniu" yaml:"qiniu"`
+	AliyunOSS  AliyunOSS  `mapstructure:"aliyun-oss" json:"aliyun-oss" yaml:"aliyun-oss"`
+	HuaWeiObs  HuaWeiObs  `mapstructure:"hua-wei-obs" json:"hua-wei-obs" yaml:"hua-wei-obs"`
+	TencentCOS TencentCOS `mapstructure:"tencent-cos" json:"tencent-cos" yaml:"tencent-cos"`
 }
 
 var config Config
--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module admin-cli
 go 1.16
 
 require (
+	github.com/aliyun/aliyun-oss-go-sdk v2.2.2+incompatible
 	github.com/casbin/casbin/v2 v2.44.1
 	github.com/casbin/gorm-adapter/v3 v3.5.1
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/flipped-aurora/gin-vue-admin/server v0.0.0-20220420024436-4dbe5ffaf97a
 	github.com/fsnotify/fsnotify v1.5.1
 	github.com/gin-gonic/gin v1.7.7
 	github.com/go-playground/locales v0.14.0
@@ -13,21 +15,22 @@ require (
 	github.com/go-playground/validator/v10 v10.10.1
 	github.com/go-redis/redis/v8 v8.11.4
 	github.com/go-sql-driver/mysql v1.6.0
+	github.com/huaweicloud/huaweicloud-sdk-go-obs v3.21.12+incompatible
 	github.com/iris-contrib/go.uuid v2.0.0+incompatible
-	github.com/jackc/pgx/v4 v4.15.0 // indirect
 	github.com/mojocn/base64Captcha v1.3.5
+	github.com/pkg/errors v0.9.1
+	github.com/qiniu/api.v7/v7 v7.8.2
 	github.com/shirou/gopsutil v3.21.11+incompatible
 	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/cast v1.4.1
 	github.com/spf13/cobra v1.4.0
 	github.com/spf13/viper v1.11.0
+	github.com/tencentyun/cos-go-sdk-v5 v0.7.34
 	github.com/tklauser/go-sysconf v0.3.10 // indirect
-	github.com/ugorji/go v1.1.13 // indirect
 	github.com/yusufpapurcu/wmi v1.2.2 // indirect
 	github.com/zeromicro/go-zero v1.3.2
-	golang.org/x/image v0.0.0-20210220032944-ac19c3e999fb // indirect
+	go.uber.org/zap v1.21.0
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 	gorm.io/driver/mysql v1.2.3
-	gorm.io/driver/postgres v1.2.3 // indirect
 	gorm.io/gorm v1.22.5
 )
